36ea949045ebefccccd46b9a8211950d41389619
paulobeckman <beckmam.paulo@hotmail.com>
2020-09-26 22:51:53 -0300

    coding update image

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -48,20 +48,49 @@ module.exports = {
         product.old_price = formatPrice(product.old_price)
         product.price = formatPrice(product.price)
 
+        //get categories
         results = await Category.all()
         const categories = results.rows
 
-        return res.render("products/edit.njk", {product, categories})
+        //get image
+        results = await Product.files(product.id)
+        let files = results.rows
+        files = files.map(file => ({
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+        }))
+
+        return res.render("products/edit.njk", {product, categories, files})
 
     },
     async put(req, res) {
         const keys = Object.keys(req.body)
 
         for(key of keys) {
-            if (req.body[key] == "") {
+            if (req.body[key] == "" && key != "removed_files") {
                 return res.send('Please, fill all fields!')
             }
         }
+
+        if(req.files.length != 0){
+            const newFilesPromise = req.files.map(file =>
+                File.create({...file, product_id: req.body.id}))
+
+            await Promise.all(newFilesPromise)
+        }
+
+        if (req.body.removed_files){
+
+            const removedFiles = req.body.removed_files.split(",")
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
+        }
+
+
 
         req.body.price = req.body.price.replace(/\D/g, "")
 
